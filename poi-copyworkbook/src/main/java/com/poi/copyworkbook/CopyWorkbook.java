067dc5807885f9a13a7af358f6e2ed028f6bd073
arun-prasath <arunprasath-dev@outlook.com>
2015-03-29 21:18:19 +0530

    Added CopyWorkbook class

---
+++ poi-copyworkbook/src/main/java/com/poi/copyworkbook/CopyWorkbook.java
@@ -0,0 +1,52 @@
+package com.poi.copyworkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class CopyWorkbook {
+	
+	public void printContents(File file) {
+		FileInputStream fis = null;
+		XSSFWorkbook workbook = null;
+		
+		try {
+			fis = new FileInputStream(file);
+			
+			workbook = new XSSFWorkbook (fis);
+			XSSFSheet sheet = workbook.getSheetAt(0);
+			 
+			Iterator<Row> rowIterator = sheet.iterator();
+			while(rowIterator.hasNext()) {
+				Row row = rowIterator.next();
+				Iterator<Cell> cellIterator = row.cellIterator();
+				while(cellIterator.hasNext()) {
+					Cell cell = cellIterator.next();
+					System.out.println(cell.getStringCellValue());
+				}
+				System.out.println();
+			}
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} finally {
+			try {
+				workbook.close();
+				fis.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
+}
